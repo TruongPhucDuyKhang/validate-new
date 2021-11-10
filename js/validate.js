fee30b1824d2a28791986858cdf43d3290482afe
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2021-11-10 16:40:32 +0700

    Edit error bai 1

--- js/validate.js
+++ js/validate.js
@@ -1,74 +1,87 @@
 // Get data form
-var inputElement     = document.querySelector("#form-validate");
-var formGroupElement = inputElement.querySelectorAll(".form-control");
-var result = [];
+const inputElement     = document.getElementById("form-validate");
+const formGroupElement = inputElement.querySelectorAll(".form-control");
 // Get input form
-var click   = document.getElementById("js_add");
-var refresh = document.getElementById("js_refresh");
-var files    = document.getElementById("js_add_avatar");
+const click   = document.getElementById("js_add");
+const refresh = document.getElementById("js_refresh");
+const files    = document.getElementById("js_add_avatar");
 // Regular expressions
-var isEmail    = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-var isPhone    = /((09|03|07|08|05)+([0-9]{8})\b)/;
-var isPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-var isBirthday = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-function removeAscent (str) {
-    if (str === null || str === undefined) return str;
-    // str = str.toLowerCase();
-    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
-    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
-    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
-    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
-    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
-    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
-    str = str.replace(/đ/g, "d");
-    return str;
-  }
-  function isValid (string) {
-    var re = /^[a-zA-Z\(^.\&*\)\(0-9!@#\$%+=./;_-]{2,}$/g; // regex here
-    return re.test(removeAscent(string));
-  }
-
-//Hàm sẽ tự động ghi chữ cái in hoa đầu tiên sau mỗi dấu space
-function jsUcfirst(string) {
-    var splitStr = string.toLowerCase().split(" ");
-    for(var i = 0; i < splitStr.length; i++){
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1);
-    }
-    return splitStr.join(' ');
-}
+// |\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\{|\}|\[|\]|\;|\:|\'|\"|\,|\.|\.|\/
+const fullnameRegex = /Á|À|Ạ|Ã|Ả|Ă|Ắ|Ằ|Ẳ|Ặ|Ẵ|Â|Ấ|Ầ|Ẫ|Ẩ|Ậ|É|È|Ẽ|Ẻ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ|Ì|Í|Ĩ|Ị|Ỉ|Ò|Ó|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ộ|Ổ|Ỗ|Ơ|Ớ|Ờ|Ở|Ợ|Ỡ|Ú|Ù|Ủ|Ụ|Ũ|Ư|Ứ|Ừ|Ữ|Ử|Ự|Ý|Ỳ|Ỹ|Ỵ|Ỷ|Đ|á|à|ạ|ã|ả|ă|ắ|ằ|ẳ|ặ|ẵ|â|ấ|ầ|ẫ|ẩ|ậ|é|è|ẽ|ẻ|ẹ|ê|ế|ề|ể|ễ|ệ|ì|í|ĩ|ị|ỉ|ò|ó|ỏ|õ|ọ|ô|ố|ồ|ộ|ổ|ỗ|ơ|ớ|ờ|ở|ợ|ỡ|ú|ù|ủ|ụ|ũ|ư|ứ|ừ|ữ|ử|ự|ý|ỳ|ỹ|ỵ|ỷ|đ^[a-zA-Z]+([a-zA-Z]|\s)+$/;
+const emailRegex    = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const phoneRegex    = /((09|03|07|08|05)+([0-9]{8})\b)/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+const birthdayRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
 
 // Function định dạng Regular expressions
 function isRegex(inputValue, regex){
     return regex.test(inputValue);
 }
 
-// Message thông báo lỗi
-function erroMessage(input, message){
-    const formControl     = input.parentElement;
-    const text            = formControl.querySelector('.invalid-feedback');
-    input.classList.add("is-invalid");
-    input.classList.remove("is-valid");
-    text.innerText        = message;
-}
+// Check validation
+function checkValidation(value, input, text, regex, max, min){
+    const formControl = input.parentElement;
+    const textInner   = formControl.querySelector('.invalid-feedback');
+    // Kiểm tra hai giá trị max, min có tồn tại hay không
+    if(min){
+        var textMin = `min length is ${min}`;
+    }else {
+        var textMin = '';
+    }
+    if(max){
+        var textMax = `max length is ${max}`;
+    }else {
+        var textMax = '';
+    }
 
-// Message thông thành công
-function successMessage(input, message){
-    const formControl     = input.parentElement;
-    const text            = formControl.querySelector('.invalid-feedback');
-    input.classList.remove("is-invalid");
-    input.classList.add("is-valid");
-    text.innerText        = message;
+    var erroMessage = input.classList.add("is-invalid");
+    erroMessage += input.classList.remove("is-valid");
+
+    if(value.trim() == ''){
+        erroMessage;
+        textInner.innerText = `${text} is required`;
+    }else if(value.trim().length < min || value.trim().length > max) {
+        erroMessage;
+        textInner.innerText = `${text} ${textMin} ${textMax} characters`;
+    }else if(!isRegex(value, regex)) {
+        erroMessage;
+        textInner.innerText = `${text} invalid`;
+    }else {
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        textInner.innerText = '';
+    }
+}
+// Check confirm password
+function checkConfirmPassword(password, confirmPassword) {
+    const input       = inputElement.confirm_password;
+    const formControl = input.parentElement;
+    const textInner   = formControl.querySelector('.invalid-feedback');
+    var erroPassword  = input.classList.add("is-invalid");
+    erroPassword += input.classList.remove("is-valid");
+
+    if(confirmPassword.trim() == '') {
+        erroPassword;
+        textInner.innerText = 'Confirm password is required';
+    }else if(password !== confirmPassword) {
+        erroPassword;
+        textInner.innerText = 'Confirm password is Invalid';
+    }else {
+        input.classList.remove("is-invalid");
+        input.classList.add("is-valid");
+        textInner.innerText = '';
+    }
 }
 
 // Định dạng form phone sau khi save
 function jsPhone(phonestr){
-    return phonestr.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3")
+    return phonestr.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
 }
 
 // Hàm reset form
-function refreshForm() {
+function refreshForms() {
     const previewImg = document.getElementById("preview-img");
-    document.getElementById("form-validate").reset();
+    inputElement.reset();
     for(var i = 0; i < formGroupElement.length; i++){
         formGroupElement[i].classList.remove("is-invalid");
         formGroupElement[i].classList.remove("is-valid");
@@ -92,105 +105,37 @@ function saveForm(result){
     resultEmail.innerText    = result.email;
     resultPhone.innerText    = jsPhone(result.phone);
     resultBirthday.innerText =  result.birthday;
+
+    // hiển thị ảnh trước khi upload
     if(previewImage){
         document.querySelector('.show-img-add').innerHTML = `<img src="${previewImage.src}" alt="" />`;
     }
 }
 
-inputElement.fullname.addEventListener('change', function(){
-    inputElement.fullname.value = jsUcfirst(inputElement.fullname.value);
-});
-
-function checkFullname(value){
-    if(value.trim() == '') {
-        erroMessage(inputElement.fullname, "Full name is required");
-    }else if(isValid(value)) {
-        erroMessage(inputElement.fullname, "Full name invalid");
-    }else if(value.trim().length < 8 || value.trim().length > 30){
-        erroMessage(inputElement.fullname, "Full name min length is 8 - max length is 30 characters");
-    }else {
-        successMessage(inputElement.fullname, '');
-    }
-}
-
-function checkEmail(value, regex){
-    if(value.trim() == '') {
-        erroMessage(inputElement.email, "Email is required");
-    }else if(!isRegex(value, isEmail)){
-        erroMessage(inputElement.email, "Email invalid");
-    }else {
-        successMessage(inputElement.email, '');
-    }
-}
-
-function checkPhone(value, regex) {
-    if(value.trim() == '') {
-        erroMessage(inputElement.phone, "Phone is required");
-    }else if(value.trim().length > 10) {
-        erroMessage(inputElement.phone, "Phone max length is 10 characters");
-    }else if(!isRegex(value, isPhone)) {
-        erroMessage(inputElement.phone, "Phone invalid");
-    }else {
-        successMessage(inputElement.phone, '');
-    }
-}
-
-function checkBirthday(value, regex){
-    if(value.trim() == '') {
-        erroMessage(inputElement.birthday, "Birthday is required");
-    }else if(!isRegex(value, isBirthday)) {
-        erroMessage(inputElement.birthday, "Birthday is Invalid");
-    }else {
-        successMessage(inputElement.birthday, '');
-    }
-}
-
-function checkPassword(value, regex) {
-    if(value.trim() == '') {
-        erroMessage(inputElement.password, "Password is required");
-    }else if(!isRegex(value, isPassword)) {
-        erroMessage(inputElement.password, "Your password must not set the character, number into the letter in uppercase");
-    }else if(value.trim().length < 8 || value.trim().length > 30) {
-        erroMessage(inputElement.password, "Invalid , character length between 8 - 30");
-    }else {
-        successMessage(inputElement.password, '');
-    }
-}
-
-function checkConfirmPassword(password, confirmPassword) {
-    if(confirmPassword.trim() == '') {
-        erroMessage(inputElement.confirm_password, "Confirm password is required");
-    }else if(password !== confirmPassword) {
-        erroMessage(inputElement.confirm_password, "Confirm password is Invalid");
-    }else {
-        successMessage(inputElement.confirm_password, '');
-    }
-}
-
 // Kiểm tra value validate on click
 function checkInputs(){
-    const fullnameValue       = inputElement.fullname.value;
-    const emailValue          = inputElement.email.value;
-    const phoneValue          = inputElement.phone.value;
-    const birthdayValue       = inputElement.birthday.value;
-    const passwordValue       = inputElement.password.value;
-    const confirmPasswordValue = inputElement.confirm_password.value;
+    const fullname       = inputElement.fullname;
+    const email          = inputElement.email;
+    const phone          = inputElement.phone;
+    const birthday       = inputElement.birthday;
+    const password       = inputElement.password;
+    const confirmPassword = inputElement.confirm_password;
 
     // Check Validate
-    checkFullname(fullnameValue);
-    checkEmail(emailValue, isEmail);
-    checkPhone(phoneValue, isPhone);
-    checkBirthday(birthdayValue, isBirthday);
-    checkPassword(passwordValue, isPassword);
-    checkConfirmPassword(passwordValue, confirmPasswordValue)
+    checkValidation(fullname.value, fullname, "Full name", fullnameRegex, 30, 8);
+    checkValidation(email.value, email, "Email", emailRegex);
+    checkValidation(phone.value, phone, "Phone", phoneRegex, 10);
+    checkValidation(birthday.value, birthday, "Birthday", birthdayRegex);
+    checkValidation(password.value, password, "Password", passwordRegex, 30, 8);
+    checkConfirmPassword(password.value, confirmPassword.value);
     
 
     // Add data form
     const result = {
-        name: fullnameValue,
-        email: emailValue,
-        phone: phoneValue,
-        birthday: birthdayValue,
+        name: fullname.value,
+        email: email.value,
+        phone: phone.value,
+        birthday: birthday.value,
     };
     saveForm(result);
 }
@@ -202,42 +147,68 @@ click.addEventListener('click', function(e){
 
 // Reset form
 refresh.addEventListener('click', function(){
-    refreshForm();
+    refreshForms();
 });
 
 // On input all
-inputElement.fullname.addEventListener('input', function(){checkFullname(this.value)});
-inputElement.email.addEventListener('input', function(){checkEmail(this.value, isEmail)});
-inputElement.phone.addEventListener('input', function(){checkPhone(this.value, isPhone)});
-inputElement.birthday.addEventListener('input', function(){checkBirthday(this.value, isBirthday)});
-inputElement.password.addEventListener('input', function(){checkPassword(this.value, isPassword)});
+inputElement.fullname.addEventListener('input', function(){checkValidation(this.value, this, "Full name", fullnameRegex, 30, 8);});
+inputElement.email.addEventListener('input', function(){checkValidation(this.value, this, "Email", emailRegex)});
+inputElement.phone.addEventListener('input', function(){checkValidation(this.value, this, "Phone", phoneRegex, 10)});
+inputElement.birthday.addEventListener('input', function(){checkValidation(this.value, this, "Birthday", birthdayRegex)});
+inputElement.password.addEventListener('input', function(){checkValidation(password.value, password, "Password", passwordRegex, 30, 8)});
 inputElement.confirm_password.addEventListener('input', function(){checkConfirmPassword(inputElement.password.value, this.value)});
+
+// Ghi hoa chữ cái đầu cho full name
+inputElement.fullname.addEventListener('change', function(){
+    const valueString = this.value;
+    const splitStr    = valueString.toLowerCase().split(" ");
+    for(let i = 0; i < splitStr.length; i++){
+        splitStr[i]  = splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1);
+    }
+    return this.value = splitStr.join(' ');
+});
 
 // Preview image files
 files.addEventListener('click', function(){
-    const inputFile = document.getElementById('upload');
+    const inputFile     = document.getElementById('upload');
+    const borderElement = document.querySelector('.preview-avatar');
+    const colorElement  = document.querySelector('.avatar i');
+    const showImage     = document.getElementById('show-image');
     inputFile.click();
     inputFile.addEventListener('change', function(e){
-        // console.log(e.target);
-        if (inputFile.files.length > 0) {
+        const file = e.target.files;
+        if (file.length > 0 && (file[0].type == "image/jpeg" ||
+                               file[0].type == "image/png" ||
+                               file[0].type == "image/jpg"))
+        {
             var fileToLoad = inputFile.files[0];
             var fileReader = new FileReader();
             fileReader.onload = function(event) {
                 var srcData  = event.target.result;
-                document.getElementById('show-image').innerHTML = `<img src="${srcData}" alt="" id="preview-img"/>`;
+                borderElement.classList.remove("is-required");
+                colorElement.classList.remove("is-required");
+                showImage.innerHTML = `<img src="${srcData}" alt="" id="preview-img"/>`;
             }
             fileReader.readAsDataURL(fileToLoad);
+        }else {
+            borderElement.classList.add("is-required");
+            colorElement.classList.add("is-required");
+            showImage.innerHTML = '';
         }
     });
 });
 
 // Browser key
 document.onkeyup = function(e){
+    // số trong e.which là những ký tự trên bàn phím của mình
+    const shiftButton  = 16;
+    const deleteButton = 46;
+
     switch(e.which){
-        case 16:
+        case shiftButton:
         checkInputs();
         break;
-        case 46:
+        case deleteButton:
         refreshForm();
         break;
     }
