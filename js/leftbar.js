ab4a91c166fe7b77619eb51c8d153d0f0773aeb9
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2021-11-11 08:45:16 +0700

    Deploy dropdown list 2

---
+++ js/leftbar.js
@@ -0,0 +1,24 @@
+const selectCity = document.querySelector(".select-city");
+//New branch
+selectCity.addEventListener('change', function(e){
+    const listGroup = document.querySelectorAll(".list-group-item");
+    const selected  = this.value;
+    //Auto clear khi click select qua number khác
+    listGroup.forEach(city => city.listClass.remove("active"));
+    for(let i = 0; i < listGroup.length; i++){
+        if(selected == "even"){
+            if(i % 2 !== 0){
+                listGroup[i].classList.add("active");
+            }
+        }else if(selected == "odd"){
+            if(i % 2 === 0){
+                listGroup[i].classList.add("active");
+            }
+        }else if(selected == "" || selected == "reset"){
+            listGroup[i].classList.remove("active");
+        }else {
+            //selected - 1 là để so sanh cho giống index trong vòng lập for == nhau
+            listGroup[selected - 1].classList.add("active");
+        }
+    }
+});
