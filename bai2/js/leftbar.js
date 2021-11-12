fe8f7b0452eb643b006bb1b7438f50d2819017a2
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2021-11-12 08:59:18 +0700

    Update error v2

--- bai2/js/leftbar.js
+++ bai2/js/leftbar.js
@@ -5,16 +5,17 @@ selectCity.addEventListener('change', function(e){
     const selected  = this.value;
     //Auto clear khi click select qua number khác
     listGroup.forEach(city => city.classList.remove("active"));
-    for(let i = 0; i < listGroup.length; i++){
-        if(selected == "even"){
-            if(i % 2 !== 0){
+    
+    for (let i = 0; i < listGroup.length; i++) {
+        if(selected == "even") {
+            if (i % 2 !== 0) {
                 listGroup[i].classList.add("active");
             }
-        }else if(selected == "odd"){
-            if(i % 2 === 0){
+        }else if (selected == "odd") {
+            if (i % 2 === 0) {
                 listGroup[i].classList.add("active");
             }
-        }else if(selected == "" || selected == "reset"){
+        }else if (selected == "" || selected == "reset") {
             listGroup[i].classList.remove("active");
         }else {
             //selected - 1 là để so sanh cho giống index trong vòng lập for == nhau
