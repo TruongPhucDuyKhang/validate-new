cfe9543a7e49c892bbf7e37d01f5a86f734471a6
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2021-11-12 09:49:19 +0700

    Update error v3

--- bai2/js/leftbar.js
+++ bai2/js/leftbar.js
@@ -5,7 +5,9 @@ selectCity.addEventListener('change', function(e) {
     const selected = this.value;
     //Auto clear khi click select qua number khác
     listGroup.forEach(city => city.classList.remove("active"));
-    
+
+    if (selected == "" || selected == "reset") return;
+
     for (let i = 0; i < listGroup.length; i++) {
         if (selected == "even") {
             if (i % 2 !== 0) {
@@ -15,8 +17,6 @@ selectCity.addEventListener('change', function(e) {
             if (i % 2 === 0) {
                 listGroup[i].classList.add("active");
             }
-        } else if (selected == "" || selected == "reset") {
-            listGroup[i].classList.remove("active");
         } else {
             //selected - 1 là để so sanh cho giống index trong vòng lập for == nhau
             listGroup[selected - 1].classList.add("active");
